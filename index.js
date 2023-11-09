ea85440f8e593f6823d9e06e133cafad401a3e9e
Justin <justinguerrerowork@gmail.com>
2023-11-09 12:57:11 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph"); 
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob"); 
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+    return cats;
+}
+function appendCat(name){
+const allCats = [...cats, "Broom"];
+return allCats;
+}
+function prependCat(name){
+    const preCats = ["Arnold",...cats];
+    return preCats;
+}
+function removeLastCat(name){
+    const remCats =[...cats];
+    remCats.pop();
+    return remCats;
+}
+function removeFirstCat(name){
+    const firstCat = [...cats];
+    firstCat.shift();
+    return firstCat;
+}
